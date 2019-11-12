14706ff7dd5bd47dda04a7d23fd14f8be96f5840
Tajcore <45900878+Tajcore@users.noreply.github.com>
2019-11-11 21:53:02 -0500

    Added CSS JS HTML PHP(update)
    Updated CSS JS HTML AND PHP FILE FOR FINAL PUSH

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
 window.onload = function(){
-
-  avengers();
-}
-
-function avengers(){
-    var xml_http = new XMLHttpRequest();
-    xml_http.open("GET","http://localhost:8080/superheroes.php",true);
-    xml_http.send(null)
-    response = xml_http.responseText
-    console.log(response)
-    document.getElementById("srchbutton").onclick = function(){
-        alert(xml_http.responseText)
-    }} 
+   
+    document.getElementById("button").onclick = function(){
+      show_result(document.getElementById("text_Field").value)
+    }
+  }
+  
+  function show_result(str) {  
+  
+    if (window.XMLHttpRequest) {
+      xml_http=new XMLHttpRequest   ();
+    } else { 
+      xml_http=new ActiveXObject("Microsoft.XMLHTTP");
+    }
+    xml_http.onreadystatechange=function() { 
+      if (this.readyState==4 && this.status==200) {
+        document.getElementById("Results").innerHTML=this.responseText;
+  
+      }
+    }
+    xml_http.open("GET","http://localhost:8080/superheroes.php?q="+str,true);
+    xml_http.send();
+  }
--- superheroes.php
+++ superheroes.php
@@ -1,5 +1,5 @@
+  
 <?php
-
 $superheroes = [
   [
       "id" => 1,
@@ -62,11 +62,27 @@ $superheroes = [
       "biography" => "Notably powerful, Wanda Maximoff has fought both against and with the Avengers, attempting to hone her abilities and do what she believes is right to help the world.",
   ], 
 ];
-
+$q =$_GET["q"];
+if (strlen($q) > 0){
+    $result = "<h3 style = 'color : red; font-weight: bold;'>SUPERHERO NOT FOUND</h3>";
+    for ($i = 0 ; $i < count($superheroes); $i++){
+        if ($q === $superheroes[$i]["alias"] || $i === $superheroes[$i]["alias"]){
+            $result = "<h3>".$superheroes[$i]["alias"]."</h3>"."\r\n"."<h4>A.K.A ".$superheroes[$i]["name"]."</h4>"."\r\n"."\r\n"."<p>".$superheroes[$i]["biography"]."/p";
+        }
+    }
+}
+$response = $result;
 ?>
 
+
+<?php if(strlen($response) > 0): ?>
+<?echo $response ?>
+<?php endif; ?>
+
 <ul>
+<?php if(strlen($response) === 0): ?>
 <?php foreach ($superheroes as $superhero): ?>
   <li><?= $superhero['alias']; ?></li>
 <?php endforeach; ?>
+<?php endif; ?>
 </ul>
